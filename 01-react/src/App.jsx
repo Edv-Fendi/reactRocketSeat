4113d04fa1c3e9bd190d62b56c96754bf30002af
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-10-24 22:13:40 -0300

    viteInit

---
+++ 01-react/src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Edvaldo Fendi"
+        content="lorem ipsum dolor sit amet, consectetur adip"
+      />
+      <Post
+        author="Joao Brasil"
+        content="Vai neymar!!!"
+      />
+
+    </div>
+  )
+}
+
