79ce6d7fc93f6f34f0669cc11081237ef83e3c4e
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-10-25 11:34:27 -0300

    header

--- 01-react/src/App.jsx
+++ 01-react/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Edvaldo Fendi"
         content="lorem ipsum dolor sit amet, consectetur adip"
