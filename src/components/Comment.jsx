d510bbb807b94ee042e48d6ada9f31ce0f31a1a7
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-10-28 11:08:47 -0300

    comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,26 +1,26 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
-export function Comment(){
+export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/Edv-Fendi.png" />  
+            <img src="https://github.com/Edv-Fendi.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Edvaldo Fendi</strong>
-                        <time title="26 de Outubro às 10:51h" dateTime='2022-10-26 10:51:38'>Cerca de 1hr atrás</time>
-                    </div>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Edvaldo Fendi</strong>
+                            <time title="26 de Outubro às 10:51h" dateTime='2022-10-26 10:51:38'>Cerca de 1hr atrás</time>
+                        </div>
 
-                    <button title='Deletar comentário'>
-                        <Trash size={20}/>
-                    </button>
-                </header>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
 
 
-                <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
                 </div>
 
 
@@ -30,7 +30,7 @@ export function Comment(){
                         aplaudir <span>20</span>
                     </button>
                 </footer>
-            </div>       
-        </div> 
+            </div>
+        </div>
     )
 }
