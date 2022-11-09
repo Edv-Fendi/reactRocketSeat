6faf5b97b31e70dd89dfade57e4101af5be8ca36
Edvaldo de f <>
2022-11-09 18:00:14 -0300

    commentsFormat

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,39 @@
+
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR/index';
+import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
 
-export function Post({ author, publishedAt }) {
+
+
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    "O melhor do mundo"
+  
+  ])
+
+
+const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'");
+
+const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+  locale: ptBR,
+  addSuffix: true,
+
+})
+
+
+function handleCreateNewComment(){
+  event.preventDefault();
+  
+  const newCommentText = event.target.comment.value
+
+  setComments([...comments, newCommentText]);
+}
+  
   return (
     <article className={styles.post}>
       <header>
@@ -15,17 +45,28 @@ export function Post({ author, publishedAt }) {
           </div>
         </div>
 
-        <time title="26 de Outubro às 10:51h" dateTime='2022-10-26 10:51:38'>{publishedAt.toString()}</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        {publishedDateRelativeToNow} 
+        </time>
       </header>
 
       <div className={styles.content}>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p> {line.content}</p>
+          }else if (line.type === 'link') {
+            return <p> <a href="#">{line.content}</a></p>
+          }
+        })}
         
       </div>
 
-      <form className={styles.comentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.comentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder='Deixe um comentario'/>
+        <textarea 
+        name="comment"
+        placeholder='Deixe um comentario'/>
 
 
         <footer>
@@ -35,9 +76,9 @@ export function Post({ author, publishedAt }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+       {comments.map(comment => {
+        return <Comment content={comment}/>
+       })}
 
       </div>
 
