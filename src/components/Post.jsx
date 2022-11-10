b53dfac3755b9fa1a71c7faa5dbd2a1230f5a06f
Edvaldo de f <>
2022-11-10 15:59:27 -0300

    keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -16,6 +16,8 @@ export function Post({ author, publishedAt, content }) {
   
   ])
 
+  const [newCommentText, setNewCommentText] = useState('');
+
 
 const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'");
 
@@ -32,6 +34,13 @@ function handleCreateNewComment(){
   const newCommentText = event.target.comment.value
 
   setComments([...comments, newCommentText]);
+
+  setNewCommentText('')
+  
+}
+
+function handleNewCommentChange(){
+  setNewCommentText(event.target.value)
 }
   
   return (
@@ -53,9 +62,9 @@ function handleCreateNewComment(){
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p> {line.content}</p>
+            return <p key={line.content}> {line.content}</p>
           }else if (line.type === 'link') {
-            return <p> <a href="#">{line.content}</a></p>
+            return <p key={line.content}> <a href="#">{line.content}</a></p>
           }
         })}
         
@@ -66,7 +75,10 @@ function handleCreateNewComment(){
 
         <textarea 
         name="comment"
-        placeholder='Deixe um comentario'/>
+        placeholder='Deixe um comentário'
+        value={newCommentText}
+        onChange={handleNewCommentChange}
+        />
 
 
         <footer>
@@ -77,7 +89,7 @@ function handleCreateNewComment(){
 
       <div className={styles.commentList}>
        {comments.map(comment => {
-        return <Comment content={comment}/>
+        return <Comment key={comment} content={comment}/>
        })}
 
       </div>
